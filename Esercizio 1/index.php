01a6bb9c4e04b2603eb440d9b2aba4ff3fc05bea
Tommaso Figlia <tommasofiglia@gmail.com>
2021-01-28 16:14:17 +0100

    Esercizio 1 completato

---
+++ Esercizio 1/index.php
@@ -0,0 +1,15 @@
+<?php
+$name = 'Tommaso';
+$last_name = 'Figlia';
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Esercizio 1 php</title>
+  </head>
+  <body>
+    <h1>Benvenuto <?php echo $name . ' ' . $last_name; ?></h1>
+  </body>
+</html>
