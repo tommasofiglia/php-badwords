e37fd867c08585f7415e3457ba939f333c9ab826
Tommaso Figlia <tommasofiglia@gmail.com>
2021-01-28 17:06:47 +0100

    Esercizio 2 scritto ma non funzionante

---
+++ Esercizio 2/index.php
@@ -0,0 +1,18 @@
+<?php
+  $text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+  $length_text = strlen($text);
+  $bad_word = $_GET["word"];
+  $change_word = str_replace($bad_word, '***', $text)
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <p><?php echo $text ?></p>
+    <p>Lunghezza del testo: <?php echo $length_text ?> caratteri</p>
+  </body>
+</html>
