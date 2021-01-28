c887876429cfae77536adcb09227364b5712781d
Tommaso Figlia <tommasofiglia@gmail.com>
2021-01-28 17:14:51 +0100

    Piccola correzione all'esercizio 2

--- Esercizio 2/index.php
+++ Esercizio 2/index.php
@@ -2,7 +2,7 @@
   $text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
   $length_text = strlen($text);
   $bad_word = $_GET["word"];
-  echo str_replace($bad_word, '***', $text)
+  $text_censured = str_replace($bad_word, '***', $text);
 ?>
 
 <!DOCTYPE html>
@@ -12,7 +12,7 @@
     <title></title>
   </head>
   <body>
-    <p><?php echo $text ?></p>
+    <p><?php echo $text_censured ?></p>
     <p>Lunghezza del testo: <?php echo $length_text ?> caratteri</p>
   </body>
 </html>
